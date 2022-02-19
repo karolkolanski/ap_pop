fbfcc9771ffc60a53fb0bf34039d34dc8ac86c00
Karol Kolanski <gralu86@gmail.com>
2022-02-19 11:54:59 +0100

    Create an Account Page Object added

---
+++ pages/create_an_account_page.py
@@ -0,0 +1,20 @@
+from pages.base_page import BasePage
+
+class CreateAnAccountPage(BasePage):
+    """
+    Create an account Page Object
+    """
+    def choose_gender(self, gender):
+        pass
+
+    def enter_name(self, name):
+        pass
+
+    def enter_last_name(self, last_name):
+        pass
+
+    def _verify_page(self):
+        """
+        Verifies Create an Account Page
+        """
+        pass
